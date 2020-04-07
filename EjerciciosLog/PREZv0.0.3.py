96bf3352be2227a10050ed33c50fa01486f7d6e5
brian8sal <bss731@alumnos.unican.es>
2020-04-07 23:13:16 +0200

    Actualizada la interaz para introducir la nueva gráfica. Esa gráfica se comía a la tarta pues la altura de su div la marcaba el dash table. Se ha alterado el orden para que la altura la amrque la gráfica. Se les ha aportado algo de margen también. Por último, se han reordenado los elementos.

--- EjerciciosLog/PREZv0.0.3.py
+++ EjerciciosLog/PREZv0.0.3.py
@@ -4,7 +4,6 @@ import dash_core_components as dcc
 import dash_html_components as html
 import MoodleAnalysisLibrary
 
-
 prueba = (MoodleAnalysisLibrary.MoodleAnalysisLibrary("logs_G668_1819_20191223-1648.csv",
                                                       "C:/Users/sal8b/OneDrive/Escritorio/Beca", "Usuarios.csv",
                                                       ['0', '323', '231']))
@@ -31,15 +30,81 @@ app.layout = html.Div(children=[
             'color': colors['text'],
         }
     ),
+    html.Div(
+        children=[
+            html.Div(
+                className='four columns',
+                children=[
+                    html.H3(
+                        children='Usuarios no participantes',
+                        style={
+                            'textAlign': 'center',
+                            'color': colors['text'],
+                        }
+                    ),
+                    dash_table.DataTable(
+                        id='table',
+                        columns=[{"name": i, "id": i} for i in
+                                 prueba.list_nonparticipant(prueba.dataframe, prueba.dataframe_Usuarios).columns],
+                        data=prueba.list_nonparticipant(prueba.dataframe, prueba.dataframe_Usuarios).to_dict(
+                            'records'),
+                        style_header={'backgroundColor': colors['background']},
+                        style_cell={'textAlign': 'left',
+                                    'backgroundColor': 'rgb(50, 50, 50)',
+                                    'color': colors['text'],
+                                    },
+                        style_table={
+                            'maxHeight': '200px',
+                            'maxWidth': '400px',
+                            'overflowY': 'scroll'
+                        },
+                    ),
+                ],
+                style={'marginLeft': '150px'}
+            ),
+            html.Div(
+                children=[
+                    dcc.Graph(
+                        id='pie-chart-participants-users',
+                        figure={
+                            'data': [
+                                {'labels': ['Participantes', 'No Participantes'],
+                                 'values': [
+                                     prueba.num_participants_nonparticipants(prueba.dataframe,
+                                                                             prueba.dataframe_Usuarios)[
+                                         'Participantes'][0],
+                                     prueba.num_participants_nonparticipants(prueba.dataframe,
+                                                                             prueba.dataframe_Usuarios)[
+                                         'No participantes'][0]], 'type': 'pie',
+                                 'automargin': True,
+                                 'textinfo': 'none'
+                                 },
+                            ],
+                            'layout': {
+                                'title': 'Eventos por recurso',
+                                "titlefont": {
+                                    "size": 23
+                                },
+                                'plot_bgcolor': colors['background'],
+                                'paper_bgcolor': colors['background'],
+                                'font': {
+                                    'color': colors['text']
+                                }
+                            }
+                        }
+                    )], style={'display': 'inline-block'},
+            ),
+
+        ], ),
     dcc.Graph(
-        id='EventosPorRecurso',
+        id='ParticipantesPorRecurso',
         figure={
             'data': [
-                {'x': prueba.events_per_resource(prueba.dataframe)['Número de eventos'],
-                 'y': prueba.events_per_resource(prueba.dataframe)['Recurso'], 'type': 'bar', 'orientation':'h'},
+                {'x': prueba.participants_per_resource(prueba.dataframe)['Número de participantes'],
+                 'y': prueba.participants_per_resource(prueba.dataframe)['Recurso'], 'type': 'bar', 'orientation': 'h'},
             ],
             'layout': {
-                'title': 'Recursos por número de eventos',
+                'title': 'Paticipantes por recurso',
                 "titlefont": {
                     "size": 23
                 },
@@ -70,71 +135,29 @@ app.layout = html.Div(children=[
         start_date=prueba.events_per_day(prueba.dataframe)['Fecha'].min(),
         end_date=prueba.events_per_day(prueba.dataframe)['Fecha'].max(),
     ),
-    html.Div(
-        children=[
-            html.Div(
-                className='eight columns',
-                children=[
-                    dcc.Graph(
-                        figure={
-                            'data': [
-                                {'labels': ['Participantes', 'No Participantes'],
-                                 'values': [
-                                     prueba.num_participants_nonparticipants(prueba.dataframe,
-                                                                             prueba.dataframe_Usuarios)[
-                                         'Participantes'][0],
-                                     prueba.num_participants_nonparticipants(prueba.dataframe,
-                                                                             prueba.dataframe_Usuarios)[
-                                         'No participantes'][0]], 'type': 'pie',
-                                 'automargin': True,
-                                 'textinfo': 'none'
-                                 },
-                            ],
-                            'layout': {
-                                'title': 'Eventos por recurso',
-                                "titlefont": {
-                                    "size": 23
-                                },
-                                'plot_bgcolor': colors['background'],
-                                'paper_bgcolor': colors['background'],
-                                'font': {
-                                    'color': colors['text']
-                                }
-                            }
-                        }
-                    )],
-            ),
 
-            html.Div(
-                children=[
-                    html.H3(
-                        children='Usuarios no participantes',
-                        style={
-                            'textAlign': 'center',
-                            'color': colors['text'],
-                        }
-                    ),
-                    html.Div(
-                        dash_table.DataTable(
-                            id='table',
-                            columns=[{"name": i, "id": i} for i in
-                                     prueba.list_nonparticipant(prueba.dataframe, prueba.dataframe_Usuarios).columns],
-                            data=prueba.list_nonparticipant(prueba.dataframe, prueba.dataframe_Usuarios).to_dict(
-                                'records'),
-                            style_header={'backgroundColor': colors['background']},
-                            style_cell={'textAlign': 'left',
-                                        'backgroundColor': 'rgb(50, 50, 50)',
-                                        'color': colors['text'],
-                                        },
-                            style_table={
-                                'maxHeight': '200px',
-                                'overflowY': 'scroll'
-                            },
-                        ),
-                    ),
-                ],
-                style={'display': 'inline-block'}, )
-        ], style={'text-align': 'center'}),
+dcc.Graph(
+        id='EventosPorRecurso',
+        figure={
+            'data': [
+                {'x': prueba.events_per_resource(prueba.dataframe)['Número de eventos'],
+                 'y': prueba.events_per_resource(prueba.dataframe)['Recurso'], 'type': 'bar', 'orientation': 'h'},
+            ],
+            'layout': {
+                'title': 'Recursos por número de eventos',
+                "titlefont": {
+                    "size": 23
+                },
+                'yaxis': {'automargin': True},
+                'xaxis': {'automargin': True},
+                'plot_bgcolor': colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font': {
+                    'color': colors['text']
+                }
+            }
+        },
+    ),
 ])
 
 
