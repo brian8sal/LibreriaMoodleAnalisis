fabe356a0a2e09d2205f11fed0a7f6ae0a08bdfd
brian8sal <bss731@alumnos.unican.es>
2020-05-21 01:24:36 +0200

    Se añaden campos de texto en los que se informa al usuario de lo que ha de hacer, así como comprobación de ciertos errores. Si sigue complicándose la interfaz habría que plantarse el externalizarla.

--- EjerciciosLog/Prez.py
+++ EjerciciosLog/Prez.py
@@ -20,7 +20,7 @@ def clicked_btn_log():
 
 def clicked_btn_config():
     windowConfig.config = filedialog.askopenfilename(initialdir=".", title="Select file",
-                                                  filetypes=(("xlsx files", "*.xlsx"), ("all files", "*.*")))
+                                                     filetypes=(("xlsx files", "*.xlsx"), ("all files", "*.*")))
 
 
 def clicked_btn_create():
@@ -53,16 +53,30 @@ def clicked_btn_siguiente():
 
 
 def clicked_btn_skip():
-        windowCreateConfig.destroy()
+    windowCreateConfig.config = ""
+    windowCreateConfig.destroy()
+
+
+def on_closing():
+    if messagebox.askokcancel("Cancelar", "¿Seguro que quiere cancelar el análisis?"):
+        windowConfig.destroy()
 
 
 windowLog = Tk()
 windowLog.title("Prez")
 windowLog.iconbitmap("assets/favicon.ico")
 
+mensaje_log = Text(windowLog, width=40, height=14)
+mensaje_log.insert(INSERT, "Bienvenido a Prez. Le acompañaremos en la configuración de su análisis. \n"
+                           "\nEn primer lugar, pulse  'Seleccione el fichero log' y elija el fichero "
+                           "log del curso que quiera analizar. "
+                           "Si actualmente no lo tiene, acceda a la sección de informes de Moodle y descargue, en"
+                           " formato .csv, el log del curso deseado. \n"
+                           "\nUna vez seleccionado el fichero, pulse 'Siguiente'.")
 btn_log = Button(windowLog, text="Seleccione el fichero log", command=clicked_btn_log)
 btn_siguiente = Button(windowLog, text="Siguiente", command=clicked_btn_siguiente)
 
+mensaje_log.pack(fill=X)
 btn_log.pack(fill=X)
 btn_siguiente.pack(fill=X)
 
@@ -72,16 +86,20 @@ windowCreateConfig = Tk()
 windowCreateConfig.title("Prez")
 windowCreateConfig.iconbitmap("assets/favicon.ico")
 
+mensaje_create = Text(windowCreateConfig, width=40, height=14)
+mensaje_create.insert(INSERT, "Prez le permite proporcionar fichero Excel de configuración con datos adicionales del"
+                              " curso.\n \nSi actualmente no dispone de este fichero, proporcione un nombre para "
+                              "el mismo, pulse 'Crear' y uno será creado (en la carpeta de instalación de Prez)"
+                              " y utilizado automáticamente en el análisis.\n"
+                              "\nSi ya dispone de un fichero Excel de configuración pulse 'Saltar este paso'.")
 btn_create = Button(windowCreateConfig, text="Crear", command=clicked_btn_create)
 btn_skip = Button(windowCreateConfig, text="Saltar este paso", command=clicked_btn_skip)
 
-
+mensaje_create.pack(fill=X)
 txt_config_name = Entry(windowCreateConfig)
-
 txt_config_name.pack(fill=X)
 btn_create.pack(fill=X)
 btn_skip.pack(fill=X)
-
 
 windowCreateConfig.mainloop()
 
@@ -89,11 +107,17 @@ windowConfig = Tk()
 windowConfig.title("Prez")
 windowConfig.iconbitmap("assets/favicon.ico")
 
+mensaje_config = Text(windowConfig, width=40, height=14)
+mensaje_config.insert(INSERT, "Para terminar, si no ha creado un fichero de configuración en el paso previo pulse "
+                              "'Seleccione el fichero de configuración y elija el fichero on el que quiera realizar "
+                              "el análisis.\n \nUna vez creado o seleccionado el fichero de configuración, pulse "
+                              "'Aceptar' y el análisis será mostrado en su navegador web.")
+
 btn_config = Button(windowConfig, text="Seleccione el fichero de configuración", command=clicked_btn_config)
 btn_accept = Button(windowConfig, text="Aceptar", command=clicked_btn_accept)
+windowConfig.protocol("WM_DELETE_WINDOW", on_closing)
 
-txt_config_name = Entry(windowConfig)
-
+mensaje_config.pack(fill=X)
 btn_config.pack(fill=X)
 btn_accept.pack(fill=X)
 
